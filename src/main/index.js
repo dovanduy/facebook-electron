3272d37d1d74583f20daddd841f4cb3dcf19d7c9
汪均鹏 <wangjunpeng1015@163.com>
2019-10-21 17:28:15 +0800

    未完成自动更新

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,7 @@
 import { app, BrowserWindow, Menu, MenuItem, ipcMain } from 'electron'
 // import { autoUpdater } from 'electron-updater'
+import updateHandle from './update.js'
+import updateChecker from '../renderer/utils/updateChecker.js'
 const ws = require('nodejs-websocket')
 const path = require('path')
 /**
@@ -15,7 +17,7 @@ const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
@@ -34,33 +36,33 @@ function createWindow () {
 
   mainWindow.loadURL(winURL)
 
-  
   const hostname = '192.168.3.5'
   const port = '9001'
 
   const createServer = function () {
     ws.createServer(function (conn) {
       console.log('Server running at ws://%s:%d/', hostname, port);
-        conn.on("text", function(data) {
-            console.log("Received:" + data);
-            try {
-              mainWindow.webContents.send('socketMsg', data)
-            } catch (e) {
-              console.log(e)
-            }
-        });
-        conn.on("close", function(code, reason) {
-            console.log("Connection closed");
-        });
-        conn.on("error", function(err) {
-            console.log("handle err");
-        });
+      conn.on("text", function (data) {
+        console.log("Received:" + data);
+        try {
+          mainWindow.webContents.send('socketMsg', data)
+        } catch (e) {
+          console.log(e)
+        }
+      });
+      conn.on("close", function (code, reason) {
+        console.log("Connection closed");
+      });
+      conn.on("error", function (err) {
+        console.log("handle err");
+      });
     }).listen(port)
   }
   createServer()
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+  updateHandle(mainWindow)
 }
 
 /**
@@ -76,6 +78,7 @@ function createWindow () {
 // })
 app.on('ready', () => {
   createWindow()
+
   // if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
 // app.on('ready', )
@@ -107,27 +110,42 @@ ipcMain.on('win-close', function () {
 // 设备右键菜单
 ipcMain.on('showDeviceRightMenu', (event, arg) => {
   const menu = new Menu()
-  menu.append(new MenuItem({label: '设备账号信息', click: function () {
-    event.sender.send('rightMenuEvent', 'get_detail');
-  }}))
-  menu.append(new MenuItem({label: '添加定时任务', click: function () {
-    event.sender.send('rightMenuEvent', 'add_task');
-  }}))
-  menu.append(new MenuItem({label: '添加好友', click: function () {
-    event.sender.send('rightMenuEvent', 'add_friends');
-  }}))
-  menu.append(new MenuItem({label: '添加小组', click: function () {
-    event.sender.send('rightMenuEvent', 'add_group');
-  }}))
-  menu.append(new MenuItem({label: '锁屏', click: function () {
-    event.sender.send('rightMenuEvent', 'lock');
-  }}))
-  menu.append(new MenuItem({label: '解屏', click: function () {
-    event.sender.send('rightMenuEvent', 'unlock');
-  }}))
+  menu.append(new MenuItem({
+    label: '设备账号信息', click: function () {
+      event.sender.send('rightMenuEvent', 'get_detail');
+    }
+  }))
+  menu.append(new MenuItem({
+    label: '添加定时任务', click: function () {
+      event.sender.send('rightMenuEvent', 'add_task');
+    }
+  }))
+  menu.append(new MenuItem({
+    label: '添加好友', click: function () {
+      event.sender.send('rightMenuEvent', 'add_friends');
+    }
+  }))
+  menu.append(new MenuItem({
+    label: '添加小组', click: function () {
+      event.sender.send('rightMenuEvent', 'add_group');
+    }
+  }))
+  menu.append(new MenuItem({
+    label: '锁屏', click: function () {
+      event.sender.send('rightMenuEvent', 'lock');
+    }
+  }))
+  menu.append(new MenuItem({
+    label: '解屏', click: function () {
+      event.sender.send('rightMenuEvent', 'unlock');
+    }
+  }))
   const win = BrowserWindow.fromWebContents(event.sender)
   menu.popup(win)
 })
 
+ipcMain.on('update-check', (event, msg) => {
+  updateChecker();
+})
 
 
